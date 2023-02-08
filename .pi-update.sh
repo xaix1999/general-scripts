85969477beb98d9ce7484f9367281c765aa54b2e
xaix1999 <88251983+xaix1999@users.noreply.github.com>
2023-02-08 16:37:16 -0700

    Update .pi-update.sh

--- .pi-update.sh
+++ .pi-update.sh
@@ -8,9 +8,13 @@ if [ "${UPDATEQ}" = "${UPDATESTRING}" ]; then
 echo "All packages are up to date."
 else
 sudo apt upgrade -y &>/dev/null
+echo "20% Complete"
 sudo apt dist-upgrade -y &>/dev/null
+echo "40% Complete"
 sudo apt autoremove -y &>/dev/null
+echo "60% Complete"
 sudo apt autopurge -y &>/dev/null
+echo "80% Complete"
 pihole -up &>/dev/null
 echo "All updates complete."
 fi
